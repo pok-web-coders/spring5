1441d56b030adb2cd1bca0f9fe5a5ad55955485e
walterpark5193 <walterpark5193@gmail.com>
2019-07-12 00:22:56 +0900

    헬로 프로젝트

---
+++ SpringMavenProject/src/main/java/com/spring/start/StartController.java
@@ -0,0 +1,34 @@
+package com.spring.start;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class StartController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(StartController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		String welcome = "À£ÄÄÀÌ¿ä!";
+		
+		model.addAttribute("welcome", welcome);
+		
+		return "home";
+	}
+	
+}
---
+++ SpringMavenProject/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,17 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<%@ page language="java" contentType="text/HTML;charset=UTF-8" pageEncoding="UTF-8" %>
+<%
+request.setCharacterEncoding("UTF-8");
+%>
+<html>
+<head>
+	<title>PDH</title>
+</head>
+<body>
+<h1>
+	Welcome, ${welcome}
+</h1>
+
+</body>
+</html>
